0e02a9702d31cb88ca2eb3fd6989dfb3a0cd0d5d
YelenaVassilyeva <jelena.oleynik@gmail.com>
2023-07-01 17:31:00 -0400

    initial commit

---
+++ src/main/java/StaticClassesEnumExeptions/Calculator.java
@@ -0,0 +1,63 @@
+package StaticClassesEnumExeptions;
+
+
+
+public class Calculator {
+    public enum Type{
+        MULTIPLICATION,
+        SUMMARY,
+        DIVISION,
+        SUBTRACTION
+    }
+
+    public static double calculate(double a, double b, Type type){
+
+        double result=0;
+        // we can do it with if else
+//        if (type== MULTIPLICATION) {
+//            return a * b;
+//        }
+//        else if (type == SUMMARY)
+
+        // we can do it with switch case
+        try {
+
+            switch (type){
+                case MULTIPLICATION:
+                    result = a*b;
+                    break;
+                case SUMMARY:
+                    result = a+b;
+                    break;
+
+                case DIVISION:
+                    result = a/b;
+                    break;
+
+                case SUBTRACTION:
+                    result = a-b;
+                    break;
+
+                default:
+                    result =-1;
+
+            }
+        }
+
+        catch (Exception err){
+            System.out.println("Something bad has happened ");
+
+            throw err;
+
+        }
+//        finally {
+//            System.out.println("Finally block");
+//            result = -1;
+//        }
+
+
+        return result;
+    }
+}
+
+
